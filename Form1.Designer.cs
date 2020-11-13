64cbbb536a120fc920b94a2be7ec8e84c92f1df8
EsposaDoArnaldo <daysechioquetta@gmail.com>
2020-11-13 01:05:31 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Alarme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.watchLb = new System.Windows.Forms.Label();
+            this.txAcordar = new System.Windows.Forms.MaskedTextBox();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cbSoneca = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // watchLb
+            // 
+            this.watchLb.Dock = System.Windows.Forms.DockStyle.Top;
+            this.watchLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.watchLb.ForeColor = System.Drawing.Color.MediumBlue;
+            this.watchLb.Location = new System.Drawing.Point(0, 0);
+            this.watchLb.Name = "watchLb";
+            this.watchLb.Size = new System.Drawing.Size(210, 31);
+            this.watchLb.TabIndex = 1;
+            this.watchLb.Text = "Alarme";
+            this.watchLb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txAcordar
+            // 
+            this.txAcordar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txAcordar.Location = new System.Drawing.Point(71, 34);
+            this.txAcordar.Mask = "00:00:00";
+            this.txAcordar.Name = "txAcordar";
+            this.txAcordar.Size = new System.Drawing.Size(78, 26);
+            this.txAcordar.TabIndex = 6;
+            this.txAcordar.TabStop = false;
+            this.txAcordar.Leave += new System.EventHandler(this.txAcordar_Leave);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Enabled = false;
+            this.BtnStart.Image = ((System.Drawing.Image)(resources.GetObject("BtnStart.Image")));
+            this.BtnStart.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.BtnStart.Location = new System.Drawing.Point(31, 66);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(150, 41);
+            this.BtnStart.TabIndex = 7;
+            this.BtnStart.Text = "Soneca";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cbSoneca
+            // 
+            this.cbSoneca.FormattingEnabled = true;
+            this.cbSoneca.Items.AddRange(new object[] {
+            "1 minuto",
+            "5 minutos",
+            "10 minutos",
+            "15 minutos",
+            "20 minutos ",
+            "30 minutos",
+            "1 hora"});
+            this.cbSoneca.Location = new System.Drawing.Point(42, 113);
+            this.cbSoneca.Name = "cbSoneca";
+            this.cbSoneca.Size = new System.Drawing.Size(121, 21);
+            this.cbSoneca.TabIndex = 8;
+            this.cbSoneca.SelectedIndexChanged += new System.EventHandler(this.cbSoneca_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(210, 146);
+            this.Controls.Add(this.cbSoneca);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.txAcordar);
+            this.Controls.Add(this.watchLb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alarme";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label watchLb;
+        private System.Windows.Forms.MaskedTextBox txAcordar;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox cbSoneca;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Alarme
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section,
+            string key, string def, StringBuilder retVal, int size, string filePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool WritePrivateProfileString(string section, string key,
+            string value, string filePath);
+
+        #region Def_FlashWindow
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct FLASHWINFO
+        {
+            public uint cbSize;
+            public IntPtr hwnd;
+            public uint dwFlags;
+            public uint uCount;
+            public uint dwTimeout;
+        }
+        private const uint FLASHW_STOP = 0;
+        private const uint FLASHW_CAPTION = 1;
+        private const uint FLASHW_TRAY = 2;
+        private const uint FLASHW_ALL = 3;
+        private const uint FLASHW_TIMER = 4;
+        private const uint FLASHW_TIMERNOFG = 12;
+
+        #endregion
+
+        #region Def_SOM
+
+        public enum PlaySoundFlags : int
+        {
+            SND_SYNC = 0x0000,  /* play synchronously (default) */ //同步
+            SND_ASYNC = 0x0001,  /* play asynchronously */ //异步
+            SND_NODEFAULT = 0x0002,  /* silence (!default) if sound not found */
+            SND_MEMORY = 0x0004,  /* pszSound points to a memory file */
+            SND_LOOP = 0x0008,  /* loop the sound until next sndPlaySound */
+            SND_NOSTOP = 0x0010,  /* don't stop any currently playing sound */
+            SND_NOWAIT = 0x00002000, /* don't wait if the driver is busy */
+            SND_ALIAS = 0x00010000, /* name is a registry alias */
+            SND_ALIAS_ID = 0x00110000, /* alias is a predefined ID */
+            SND_FILENAME = 0x00020000, /* name is file name */
+            SND_RESOURCE = 0x00040004  /* name is resource name or atom */
+        }
+
+        [DllImport("winmm")]
+        public static extern bool PlaySound(string szSound, IntPtr hMod, PlaySoundFlags flags);
+
+        #endregion
+
+        DateTime dtAlarme;
+        Boolean Tocando = false;
+        Boolean Carregando = false;
+        Boolean InicDepoisHora = false;
+        Boolean PrimeiroTick = true;
+        int TmpToc = 0;
+
+        #region Eventos
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                Carregando = true;
+                txAcordar.Text = LeIni("Hora", "07:20:00");
+
+                // txAcordar.Text = "14:28:00";
+                dtAlarme = Convert.ToDateTime(txAcordar.Text);
+
+                cbSoneca.SelectedIndex = int.Parse(LeIni("Soneca", "0"));
+            }
+
+            private void timer1_Tick(object sender, EventArgs e)
+        {
+            Carregando = false;
+            TimeSpan span = DateTime.Now.Subtract(dtAlarme);
+            if (Tocando)
+            {
+                TmpToc++;
+                if (TmpToc > 2)
+                    TocaSom();
+            }
+            else
+            {
+
+                if (DateTime.Now > dtAlarme)
+                {
+                    if (PrimeiroTick)
+                    {
+                        dtAlarme = dtAlarme.AddDays(1);
+                    }
+                    else
+                    {
+                        BtnStart.Enabled = true;
+                        cbSoneca.Enabled = true;
+                        this.TopMost = true;
+                        TocaSom();
+                        FazFlash();
+                        if (this.WindowState == FormWindowState.Minimized)
+                            this.WindowState = FormWindowState.Normal;
+                    }
+                }
+            }
+            PrimeiroTick = false;
+            String sTempo = span.ToString();
+            watchLb.Text = sTempo.Substring(1, 8);
+        }
+
+        private void TocaSom()
+            {
+                TmpToc = 0;
+                Tocando = true;
+                string s = Application.StartupPath + "\\galo.wav";
+                PlaySound(s, IntPtr.Zero, PlaySoundFlags.SND_FILENAME | PlaySoundFlags.SND_ASYNC | PlaySoundFlags.SND_NODEFAULT);
+            }
+
+            private void BtnStart_Click(object sender, EventArgs e)
+            {
+                TiraFlash();
+                BtnStart.Enabled = false;
+                cbSoneca.Enabled = false;
+                dtAlarme = HoraPraTocar(DateTime.Now);
+                txAcordar.Text = dtAlarme.ToLongTimeString();
+                Tocando = false;
+
+            }
+
+            private void txAcordar_Leave(object sender, EventArgs e)
+            {
+                EscreveINI("Hora", txAcordar.Text);
+                dtAlarme = Convert.ToDateTime(txAcordar.Text);
+            }
+
+            private void cbSoneca_SelectedIndexChanged(object sender, EventArgs e)
+            {
+                if (Carregando == false)
+                {
+                    EscreveINI("Soneca", cbSoneca.SelectedIndex.ToString());
+                }
+            }
+
+        #endregion
+
+        private DateTime HoraPraTocar(DateTime HoraINI)
+        {
+            int QtdSegSon = 0;
+            int iSoneca = cbSoneca.SelectedIndex;            
+            switch (iSoneca)
+            {
+                case 1: // 5 minutos
+                    QtdSegSon = 5 * 60;
+                    break;
+                case 2: // 10 minutos
+                    QtdSegSon = 10 * 60;
+                    break;
+                case 3: // 15 minutos
+                    QtdSegSon = 15 * 60;
+                    break;
+                case 4: // 20 minutos
+                    QtdSegSon = 20 * 60;
+                    break;
+                case 5: // 30 minutos
+                    QtdSegSon = 30 * 60;
+                    break;
+                case 6: //  1 hora
+                    QtdSegSon = 60 * 60;
+                    break;
+                default: // 1 minuto
+                    QtdSegSon = 60;
+                    break;
+            }
+            DateTime HoraTocar = HoraINI.AddSeconds(QtdSegSon);
+            return HoraTocar;
+        }
+
+        #region Flash
+        private void FazFlash()
+        {
+            IntPtr hWnd = this.Handle;
+            FLASHWINFO fInfo = new FLASHWINFO();
+
+            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+            fInfo.hwnd = hWnd;
+            fInfo.dwFlags = FLASHW_ALL | FLASHW_TIMER; // FLASHW_TIMERNOFG;
+            fInfo.uCount = UInt32.MaxValue;
+            fInfo.dwTimeout = 0;
+            FlashWindowEx(ref fInfo);
+        }
+
+        private void TiraFlash()
+        {
+            IntPtr hWnd = this.Handle;
+            FLASHWINFO fInfo = new FLASHWINFO();
+
+            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+            fInfo.hwnd = hWnd;
+            fInfo.dwFlags = FLASHW_STOP;
+            fInfo.uCount = UInt32.MaxValue;
+            fInfo.dwTimeout = 0;
+            FlashWindowEx(ref fInfo);
+        }
+
+        #endregion
+
+        #region INI
+        private String LeIni(String Key, String Default)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString("Alarme", Key, Default, temp, 255, Application.StartupPath + "\\Alarme.ini");
+            return temp.ToString();
+        }
+
+        private void EscreveINI(String Key, String Texto)
+        {
+            WritePrivateProfileString("Alarme", Key, Texto, Application.StartupPath + "\\Alarme.ini");
+        }
+        #endregion
+
+    }
+}
